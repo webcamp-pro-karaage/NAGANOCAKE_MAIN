b6d2964db3da3c261b3d9599c570463cbf8dae42
funxxfun <23shirasawa@gmail.com>
2021-08-22 06:12:11 +0000

    [Add]admin_items

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2b27aec912cc39b7d8243521ef105c77552968f502986491447c693f3be3964bf5ca12d70fa524c4bcb0a8eeeb8b9c6d7587ba72da2822546d48e58253e2ea51'
